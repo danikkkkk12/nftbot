770ffcd864d27ef72665dad83476461bfb2dceed
bcheban <128683111+bcheban@users.noreply.github.com>
2025-05-15 23:16:40 +0300

    commented the whole nav-bar code (currently)

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,20 +1,20 @@
-const navItems = document.querySelectorAll('.icon-navigation-bar .nav-item');
+// const navItems = document.querySelectorAll('.icon-navigation-bar .nav-item');
 
-    function setActiveItem(clickedItem) {
-        navItems.forEach(item => {
-            if (item !== clickedItem) {
-                item.classList.remove('active-item');
-            }
-        });
+//     function setActiveItem(clickedItem) {
+//         navItems.forEach(item => {
+//             if (item !== clickedItem) {
+//                 item.classList.remove('active-item');
+//             }
+//         });
 
-        clickedItem.classList.add('active-item');
-    }
+//         clickedItem.classList.add('active-item');
+//     }
 
-    navItems.forEach(item => {
-        item.addEventListener('click', function(event) {
-            if (this.getAttribute('href') === '#') {
-                event.preventDefault();
-            }
-            setActiveItem(this);
-        });
-    });
+//     navItems.forEach(item => {
+//         item.addEventListener('click', function(event) {
+//             if (this.getAttribute('href') === '#') {
+//                 event.preventDefault();
+//             }
+//             setActiveItem(this);
+//         });
+//     });
