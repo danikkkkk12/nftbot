ed4acd62e1728222b967ea854e712dfe28fc0628
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-05-16 14:43:14 +0300

    fix nav-bar bugs

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,20 +1,33 @@
-const navItems = document.querySelectorAll('.icon-navigation-bar .nav-item');
+const navItems = document.querySelectorAll(".icon-navigation-bar .nav-item");
 
-    function setActiveItem(clickedItem) {
-        navItems.forEach(item => {
-            if (item !== clickedItem) {
-                item.classList.remove('active-item');
-            }
-        });
+function setActiveItem(clickedItem) {
+  navItems.forEach((item) => {
+    if (item !== clickedItem) {
+      item.classList.remove("active-item");
+    }
+  });
 
-        clickedItem.classList.add('active-item');
+  clickedItem.classList.add("active-item");
+}
+
+const sections = document.querySelectorAll("section");
+
+navItems.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    const targetId = event.currentTarget.id;
+    console.log(targetId, event.currentTarget);
+
+    sections.forEach((section) => {
+      section.style.display = "none";
+    });
+
+    if (targetId === "profile") {
+      document.querySelector(".user-page").style.display = "block";
+    } else if (targetId === "rocket") {
+      document.querySelector(".main-rocket").style.display = "block";
+      document.querySelector(".down-main").style.display = "block";
     }
 
-    navItems.forEach(item => {
-        item.addEventListener('click', function(event) {
-            if (this.getAttribute('href') === '#') {
-                event.preventDefault();
-            }
-            setActiveItem(this);
-        });
-    });
+    setActiveItem(this);
+  });
+});
