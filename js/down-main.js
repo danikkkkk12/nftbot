7342589bfef1f8fc5f6021312ec3e983f589ed93
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-05-17 22:40:20 +0300

    Created functional in bet for down main
    Co-Authored-By: OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>

--- js/down-main.js
+++ js/down-main.js
@@ -1,9 +1,13 @@
 const toggleButtons = document.querySelectorAll(".bal-inv__btn");
 const betToggle = document.querySelector(".bet-Toggle");
-const giftCard = document.querySelectorAll('.inventory-skins-items-card')
-const userNames = document.querySelector('.user-page-profile__name')
+const giftCard = document.querySelectorAll('.select-bet-change__btn')
 const betContainer = document.querySelector('.bet-count-list') 
+const userNames = document.querySelector('.user-page-profile__name');
 const userProfImg = document.querySelector('.user-page-profile__avatar')
+const totalBet = document.getElementById('total')
+const fieldBet = document.querySelectorAll(".select-bet-count__number");
+const betBtn = document.querySelectorAll('.select-bet__btn')
+
 
 
 
@@ -34,30 +38,83 @@ if (betToggle) {
   console.warn("Элемент .bet-toggle не найден");
 }
 
+const changeBet = function (field, fixedBtns, changeBtns) {
+  let currentValue = Number(field.textContent) || 0;
+  let currentOperation = "";
 
-giftCard.forEach((e) => {
-  e.addEventListener('click', () => {
-const betValueEl = e.querySelector('.inventory-skins-items-card__current');
-const betValue = betValueEl ? betValueEl.textContent.trim() : '';
+  changeBtns.forEach((el) => {
+    el.addEventListener("click", () => {
+      currentOperation = el.id;
+      console.log(currentOperation);
+    });
+  });
+
+  fixedBtns.forEach((el) => {
+    el.addEventListener("click", () => {
+      if (currentOperation === "plus") {
+        currentValue += Number(el.textContent);
+      } else if (
+        currentOperation === "minus" &&
+        currentValue >= Number(el.textContent)
+      ) {
+        currentValue -= Number(el.textContent);
+      }
+
+      field.textContent = currentValue;
+    });
+  });
+
+  return currentValue;
+}
+
+betBtn.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    const betValue = Number(fieldBet[index].textContent); 
+    const userName = userNames.textContent.trim();
+    const userAvatar = userProfImg.src;
+
     betContainer.innerHTML += `
-    <li class="swiper-slide bet-count-list__item">
-            <div class="bet-count-list__profile">
-            <img src="${userProfImg.src}" class="bet-count-list__avatar" alt="avatar">
-              <h3 class="bet-count-list__username">${userNames.textContent}</h3>
-            </div>
-            <div class="bet-count-list__number">
-            <span class="count">${betValue}</span>
+       <li class="swiper-slide bet-count-list__item">
+        <div class="bet-count-list__profile">
+          <img src="${userAvatar}" class="bet-count-list__avatar" alt="avatar">
+          <h3 class="bet-count-list__username">${userName}</h3>
+        </div>
+        <div class="bet-count-list__number">
+          <span class="count">${betValue}</span>
+          <img src="web/images/main/ton-icon.svg" alt="diamond" class="bet-count-list__diamond" />
+        </div>
+      </li>
+    `;
+
+      const total = document.querySelectorAll('.bet-count-list__item').length;
+    totalBet.textContent = total;
+  });
+});
+
+// giftCard.forEach((e) => {
+//   e.addEventListener('click', () => {
+//     betContainer.innerHTML += `
+//     <li class="swiper-slide bet-count-list__item">
+//             <div class="bet-count-list__profile">
+//             <img src="${userProfImg.src}" class="bet-count-list__avatar" alt="avatar">
+//               <h3 class="bet-count-list__username">${userNames.textContent}</h3>
+//             </div>
+//             <div class="bet-count-list__number">
+//             <span class="count">${betValue}</span>
               
-              <img
-                src="web/images/main/ton-icon.svg"
-                alt="diamond"
-                class="bet-count-list__diamond"
-              />
-            </div>
-          </li>
-    `
-  })
-})
+//               <img
+//                 src="web/images/main/ton-icon.svg"
+//                 alt="diamond"
+//                 class="bet-count-list__diamond"
+//               />
+//             </div>
+//           </li>
+//     `
+
+//     const currentCount = betContainer.querySelectorAll('.bet-count-list__item').length;
+//     totalBet.textContent = currentCount
+//   })
+// })
 
 
 
