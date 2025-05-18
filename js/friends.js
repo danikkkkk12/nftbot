29e6a3bb28fa3b55f2992d2b8a3ec4ea7483cab8
bcheban <128683111+bcheban@users.noreply.github.com>
2025-05-18 22:20:35 +0300

    added function for invation in section 'friends'

--- js/friends.js
+++ js/friends.js
@@ -21,3 +21,16 @@
                     alert('Не удалось скопировать ссылку. Попробуйте вручную.');
                 });
             });
+
+            const inviteMainButton = document.querySelector('.invite-button-main');
+
+if (inviteMainButton) {
+    inviteMainButton.addEventListener('click', function() {
+        const referralLink = window.location.href;
+        const shareMessage = "Привет! Получи 10% от депозита вашего друга, присоединяйся через мою ссылку!";
+
+        const telegramShareUrl = `https://t.me/share/url?url=${encodeURIComponent(referralLink)}&text=${encodeURIComponent(shareMessage)}`;
+
+        window.open(telegramShareUrl, '_blank');
+    });
+}
