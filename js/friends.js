0ff6aa8155869d91bdfb2d222d68c3f248f4cdd0
danikkkkk12 <danilmalyk20@gmail.com>
2025-05-19 13:31:13 +0300

    fix link blyat...

--- js/friends.js
+++ js/friends.js
@@ -4,7 +4,7 @@ const notification = document.getElementById("copyNotification");
 if (copyButton) {
     copyButton.addEventListener("click", async function () {
         try {
-            await navigator.clipboard.writeText("tg://resolve?domain=danikkkkk12_nftbot&startapp=");
+            await navigator.clipboard.writeText("https://t.me/danikkkkk12_nftbot/webapp");
             notification.classList.add("show");
 
             setTimeout(() => notification.classList.remove("show"), 2000);
@@ -19,7 +19,7 @@ const inviteMainButton = document.querySelector(".invite-button-main");
 
 if (inviteMainButton) {
     inviteMainButton.addEventListener("click", function () {
-        const telegramDeepLink = "tg://resolve?domain=danikkkkk12_nftbot&startapp=";
+        const telegramDeepLink = "https://t.me/danikkkkk12_nftbot/webapp";
         const shareMessage = "Привет! Получи 10% от депозита вашего друга, присоединяйся через мою ссылку!";
 
         window.open(`https://t.me/share/url?url=${encodeURIComponent(telegramDeepLink)}&text=${encodeURIComponent(shareMessage)}`, "_blank");
