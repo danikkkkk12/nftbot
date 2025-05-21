2484660133a815b8e613eb145f1320375a0a2b28
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-05-21 13:49:40 +0300

    add admin logic

---
+++ js/admin.js
@@ -0,0 +1,124 @@
+const addAdminInput = document.querySelector(".admin-add__input-add");
+const addAdminBtn = document.querySelector(".admin-add__btn--add");
+const removeAdminInput = document.querySelector(".admin-add__input--remove");
+const removeAdminBtn = document.querySelector(".admin-add__btn--remove");
+const openAdminPage = document.querySelector(".user-page-inv__icon--admin");
+const adminSection = document.querySelector(".admin");
+import { getTelegramId } from "./profile.js";
+
+const isUserAdmin = async function () {
+  const tgId = getTelegramId();
+
+  try {
+    const response = await fetch("https://nftbotserver.onrender.com/api/users");
+    const users = await response.json();
+
+    const user = users.find((user) => user.telegramId === tgId);
+
+    if (user && user.isAdmin) {
+      return user;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.log("Помилка при перевірці адміністратора:", err);
+    return false;
+  }
+};
+const addAdmins = async function (userId) {
+  try {
+    const response = await fetch("https://nftbotserver.onrender.com/api/users");
+    if (!response.ok)
+      throw new Error("Не вдалося отримати список користувачів");
+
+    const users = await response.json();
+    const user = users.find((u) => String(u.telegramId) === String(userId));
+
+    if (!user) {
+      return alert("Ви ввели невірний ID");
+    }
+
+    if (user.isAdmin) {
+      return alert("Користувач вже є адміністратором");
+    }
+
+    const updateRes = await fetch(
+      `https://nftbotserver.onrender.com/api/users/${user.telegramId}`,
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ isAdmin: true }),
+      }
+    );
+
+    if (updateRes.ok) {
+      alert("Користувача успішно призначено адміністратором");
+    } else {
+      alert("Помилка при оновленні користувача");
+    }
+  } catch (err) {
+    console.error("Помилка при додаванні адміністратора:", err);
+  }
+};
+
+const removeAdmins = async function (userId) {
+  try {
+    const response = await fetch("https://nftbotserver.onrender.com/api/users");
+    if (!response.ok)
+      throw new Error("Не вдалося отримати список користувачів");
+
+    const users = await response.json();
+    const user = users.find((u) => String(u.telegramId) === String(userId));
+
+    if (!user) {
+      return alert("Ви ввели невірний ID");
+    }
+
+    if (!user.isAdmin) {
+      return alert("Користувач не є адміністратором");
+    }
+
+    const updateRes = await fetch(
+      `https://nftbotserver.onrender.com/api/users/${user.telegramId}`,
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ isAdmin: false }),
+      }
+    );
+
+    if (updateRes.ok) {
+      alert("Користувача успішно позбавлено прав адміністратора");
+    } else {
+      alert("Помилка при оновленні користувача");
+    }
+  } catch (err) {
+    console.error("Помилка при знятті адміністратора:", err);
+  }
+};
+
+const showSection = function (targetSection) {
+  document.querySelectorAll("section").forEach((section) => {
+    section.style.display = "none";
+  });
+  targetSection.style.display = "block";
+};
+
+isUserAdmin().then((user) => {
+  if (user) {
+    openAdminPage.style.display = "block";
+  } else {
+    openAdminPage.style.display = "none";
+  }
+});
+openAdminPage.addEventListener("click", () => {
+  showSection(adminSection);
+});
+addAdminBtn.addEventListener("click", () => {
+  const id = Number(addAdminInput.value);
+  addAdmins(id);
+});
+removeAdminBtn.addEventListener("click", () => {
+  const id = Number(removeAdminInput.value);
+  addAdmins(id);
+});
