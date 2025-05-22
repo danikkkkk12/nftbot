b75612855d93385ba276888c877f56ce959e05c7
Exohspoer <kakashihatake6hokagegygg@gmail.com>
2025-05-22 13:16:23 +0300

    created deposit

--- js/ton.js
+++ js/ton.js
@@ -3,8 +3,11 @@ const mainConnectWallet = document.querySelector(".main-connect-wallet");
 const modal = document.getElementById("modal");
 const openBtn = document.getElementById("openModalBtn");
 const closeBtn = document.getElementById("closeModalHeaderBtn");
-const modalForm = document.querySelector(".modal-form");
-const sumPay = document.getElementById("sumPay");
+
+const btnTon = document.querySelector('.modal-btn-container-ton')
+const btnCryptoBot = document.querySelector('.modal-btn-container-cryptoBot')
+const btnTonContainer = document.querySelector('.modal-container-ton')
+const btnCryptoBotContainer = document.querySelector('.modal-container-cryptoBot')
 
 const tonConnect = new TON_CONNECT_UI.TonConnectUI({
   manifestUrl: "https://danikkkkk12.github.io/nftbot/tonconnect-manifest.json",
@@ -67,6 +70,83 @@ tonConnect.onStatusChange(async (walletInfo) => {
 });
 
 
+
+btnTon.addEventListener('click', () => {
+  btnCryptoBotContainer.innerHTML = ``
+
+  btnTon.classList.toggle('btnActive')
+
+  btnTonContainer.innerHTML = `
+    <form class="modal-form-ton"> 
+      <label class="label" for="sumPay">Choose sum pay</label>
+      <input type="number" name="sumPay" id="sumPay-ton" min="0.00" required />
+      <div class="modal-form-reqeired">
+        <span>Min: 0.1 TON</span>
+        <span>Max: 1000 TON</span> 
+      </div>
+      <button class="btn-ton" type="submit">Deposit with TON</button>
+    </form>
+  `;
+
+  const modalFormTon = document.querySelector(".modal-form-ton");
+  const sumPayTon = document.getElementById("sumPay-ton");
+
+  modalFormTon.addEventListener("submit", () => {
+
+    if (!sumPayTon) {
+      alert("Поле суммы не найдено");
+      return;
+    }
+
+    const amountTon = parseFloat(sumPayTon.value);
+    if (isNaN(amountTon) || amountTon <= 0) {
+      alert("Введите корректную сумму");
+      return;
+    }
+
+    const wallet = "UQBbEo60L7OU5bSFFpo9t10whVNDqtyo2lsvRJzIBhI-0l75";
+    const amountNanoTon = amountTon * 1e9;
+    const url = `https://tonhub.com/transfer/${wallet}?amount=${amountNanoTon}`;
+
+    window.open(url, "_blank");
+  });
+});
+
+
+btnCryptoBot.addEventListener('click', () => {
+  btnCryptoBotContainer.innerHTML = `
+    <form class="modal-form-cryptoBot"> 
+      <label class="label" for="sumPayCryptoBot">Choose sum pay</label>
+      <input type="number" name="sumPay" id="sumPayCryptoBot" min="0.00" required />
+      <div class="modal-form-reqeired">
+        <span>Min: 0.1 TON</span>
+        <span>Max: 1000 TON</span> 
+      </div>
+      <button class="btn" type="submit">Deposit with Crypto Bot</button>
+    </form>`;
+
+  btnTonContainer.innerHTML = ``;
+
+  const modalFormCrypto = document.querySelector(".modal-form-cryptoBot");
+  const sumPayCrypto = document.getElementById("sumPayCryptoBot");
+
+  modalFormCrypto.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const amount = parseFloat(sumPayCrypto.value);
+    if (isNaN(amount) || amount <= 0) {
+      alert("Введите корректную сумму");
+      return;
+    }
+
+    const currency = "TON";
+    const url = `https://t.me/CryptoBot?start=send_${currency}_${amount}`;
+
+    window.open(url, "_blank");
+  });
+});
+
+
 function toggleActive() {
   if (modal) {
     modal.classList.toggle("activess");
@@ -81,25 +161,10 @@ if (closeBtn) {
   closeBtn.addEventListener("click", toggleActive);
 }
 
-if (modalForm) {
-  modalForm.addEventListener("submit", (event) => {
-    event.preventDefault();
 
-    if (!sumPay) {
-      alert("Поле суммы не найдено");
-      return;
-    }
+setInterval(() => {
+  if (tonConnect.wallet && tonConnect.wallet.account) {
+    updateBalance();
+  }
+}, 10000);
 
-    const amountTon = parseFloat(sumPay.value);
-    if (isNaN(amountTon) || amountTon <= 0) {
-      alert("Введите корректную сумму");
-      return;
-    }
-
-    const wallet = "UQBbEo60L7OU5bSFFpo9t10whVNDqtyo2lsvRJzIBhI-0l75";
-    const amountNanoTon = amountTon * 1e9;
-    const url = `https://tonhub.com/transfer/${wallet}?amount=${amountNanoTon}`;
-
-    window.open(url, "_blank");
-  });
-}
